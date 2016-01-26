48658b3f0f60b6f47f20eba99c429fbd06146fa2
Sweetseller <dearaujo.matthieu@outlook.fr>
2016-01-26 17:57:41 +0100

    updated comments for Pixel.java

--- app/src/main/java/pg/data/Pixel.java
+++ app/src/main/java/pg/data/Pixel.java
@@ -2,9 +2,7 @@ package pg.data;
 
 /**
  * A class to handle pixel in projective geometry
- * 
  * @author Cedric Telegone, ECN 2010
- * 
  */
 public class Pixel {
 	protected int x;
@@ -12,51 +10,45 @@ public class Pixel {
 
 	/**
 	 * create a pixel from his x and y coordinates
-	 * 
-	 * @param x
-	 *            - x coordinate
-	 * @param y
-	 *            - y coordinate
+	 * @param x coordiante
+	 * @param y coordiante
 	 */
 	public Pixel(int x, int y) {
-		// construction de la repr�sentation homog�ne d'un point � partir de sa
-		// repr�sentation inhomog�ne
+		// translates from the homogeneous representation to the inhomogeneous' one
 		this.x = x;
 		this.y = y;
 	}
 
 	/**
+	 * Homogeneous Vector's Getter
 	 * get an homogeneous vector from the pixel coordinates
-	 * 
 	 */
 	public Vector getVec() {
 		return new Vector((double) x, (double) y, 1);
 	}
 
 	/**
+	 * X coordinate's Getter
 	 * get x coordinate
-	 * 
 	 */
 	public int getX() {
 		return x;
 	}
 
 	/**
+	 * Y coordiante's Getter
 	 * get y coordinate
-	 * 
 	 */
 	public int getY() {
 		return y;
 	}
 
 	/**
-	 * Transform a pixel into Point
-	 * 
+	 * Transform a pixel into a Point
 	 * @return the transformed Point
 	 */
 	public Point toPoint() {
 		return new Point(this.getVec());
-
 	}
 
 }
