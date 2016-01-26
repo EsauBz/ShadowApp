98842a06c84c9e022183fa15b285889dacad2807
Sweetseller <dearaujo.matthieu@outlook.fr>
2016-01-26 17:52:09 +0100

    added comments for the class Calculs.java

--- app/src/main/java/com/ei3info/gsun/Calculs.java
+++ app/src/main/java/com/ei3info/gsun/Calculs.java
@@ -1,20 +1,29 @@
 package com.ei3info.gsun;
-public class Calculs {
-	
-	//class which allows to provide Solar Azimuth and Solar Height
-	//depending on the user position and the date
+
+/**
+ * class referring to Solar Azimuth and Solar Height
+ */
+public class 	Calculs {
+
+	//this class depends on the user position and the date
 	private PositionUtilisateur position;
 	private Temps date;
-	
-	
-	//constructor with fields
+
+	/**
+	 * constructor using user's GPS positioning and date
+	 * @param position
+	 * @param date
+	 */
 	public Calculs(PositionUtilisateur position, Temps date) {
 		this.position = position;
 		this.date = date;
 	}
-	
-	//Return the time difference (in hour) between a civil day (24) and the solar day duration (variable
-	//according to the specific day
+
+	/**
+	 * Returns the time difference (in hour) between a civil day (24) and the solar day duration (variable
+	 * according to the specific day
+	 * @return Time Difference
+	 */
 	public double EquationTemps(){
 		//average anomaly in degrees
 		double M = (357 + 0.9856 * date.getQuantiemeAnnee())*Math.PI/180;
@@ -30,8 +39,11 @@ public class Calculs {
 		ET /= 60;
 		return ET;
 	}
-	
-	// it is a parameter necessary to compute the rise and the fall of the sun 
+
+	/**
+	 * Returns the parameter necessary to compute the rise and the fall of the sun
+	 * @return Rise and Fall parameter
+	 */
 	public double parametreLeverCoucher(){
 		double H1 = (-0.01454 - Math.sin(date.getDeclinaisonSolaire()*Math.PI/180) * Math.sin(position.getLatitude()*Math.PI/180)) / (Math.cos(date.getDeclinaisonSolaire()*Math.PI/180) * Math.cos(position.getLatitude()*Math.PI/180));
 		double H0 =0;
@@ -45,8 +57,11 @@ public class Calculs {
 		H0 /= 15;
 		return H0;
 	}
-	
-	// Returns the Solar time, necessary to compute the Azimuth and the Height
+
+	/**
+	 * Returns the Solar time, necessary to compute the Azimuth and the Height
+	 * @return Real Solar Time
+	 */
 	public double tempsSolaireVrai(){
 		//temps solaire moyen
 		double TSM = date.getHeure() + position.getLongitude()/15;
@@ -55,15 +70,20 @@ public class Calculs {
 		
 		return TSV;
 	}
-	
-	
-	// Returns the hour angle
+
+	/**
+	 * Returns the hour angle
+	 * @return Hour Angle
+	 */
 	public double angleHoraire(){
 		double AH = 15*(this.tempsSolaireVrai() - 12);
 		return AH;
 	}
-	
-	// Returns the Solar Azimuth
+
+	/**
+	 * Returns the Solar Azimuth
+	 * @return Solar Azimuth
+	 */
 	public double getAzimut(){
 		double num = Math.sin(position.getLatitude()*Math.PI/180) * Math.cos(date.getDeclinaisonSolaire()*Math.PI/180) * Math.cos(this.angleHoraire()*Math.PI/180) - Math.cos(position.getLatitude()*Math.PI/180) * Math.sin(date.getDeclinaisonSolaire()*Math.PI/180);
 		double cosA = (num)/(Math.cos(this.getHauteurSolaire()*Math.PI/180));
@@ -76,15 +96,21 @@ public class Calculs {
 		}
 		return Azimut;
 	}
-	
-	// Returns the Solar Height 
+
+	/**
+	 * Returns the Solar Height
+	 * @return Solar Height
+	 */
 	public double getHauteurSolaire(){
 		double sinH = Math.sin(position.getLatitude()*Math.PI/180) * Math.sin(date.getDeclinaisonSolaire()*Math.PI/180) + Math.cos(position.getLatitude()*Math.PI/180) * Math.cos(date.getDeclinaisonSolaire()*Math.PI/180) * Math.cos(this.angleHoraire()*Math.PI/180);
 		return Math.asin(sinH) * 180/Math.PI;
 	}
-	
-	
-	// Add an hour or two according to the period ( winter or summer), from Solar time to civil time 
+
+
+	/**
+	 * Add an hour or two according to the period ( winter or summer), from Solar time to civil time
+	 * @return the current Hour Majorated
+	 */
 	public double majorationHoraire(){
 		double offset = 0;
 		if (date.mois == 7 || date.mois == 8 || date.mois == 6 && date.jour >=21 || date.mois == 9 && date.jour <=21){
@@ -95,14 +121,20 @@ public class Calculs {
 		
 		return offset;
 	}
-		
-	
-	// Returns the French time zone
+
+
+	/**
+	 * Returns the French time zone
+	 * @return French Time Zone
+	 */
 	public double fuseau(){
 		return 1;
 	}
-	
-	//Returns the hour the Sun is rising
+
+	/**
+	 * Returns the hour the Sun is rising
+	 * @return SunRising Time
+	 */
 	public double getHeureLever(){
 		//true solar hour
 		double VL = 12 - this.parametreLeverCoucher();
@@ -116,8 +148,11 @@ public class Calculs {
 		return HL;
 		//return TL;
 	}
-	
-	//Returns the hour the Sun is setting
+
+	/**
+	 * Returns the hour the Sun is setting
+	 * @return SunSetting Time
+	 */
 	public double getHeureCoucher(){
 		//true solar hour
 		double VC = 12 + this.parametreLeverCoucher();
@@ -130,9 +165,6 @@ public class Calculs {
 		double HC = TC + majorationHoraire();
 		return HC;	
 		}
-	
-	
-	
+
 
 }
-
